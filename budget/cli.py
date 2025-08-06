bccec40fba43a78d791d727e86b4514bdb1fce57
AshyAspen <48628344+AshyAspen@users.noreply.github.com>
2025-08-06 11:25:07 -0400

    Allow quitting menus with q

--- budget/cli.py
+++ budget/cli.py
@@ -51,6 +51,8 @@ def select(message, choices, default=None):
         header=message,
         footer_right=f"{bal_amt:.2f}",
     )
+    if selected is None:
+        return None
     return values[selected]
 
 
@@ -691,6 +693,8 @@ def scroll_menu(
                 return index
             elif key == ord("a") and allow_add:
                 return -1
+            elif key == ord("q"):
+                return None
 
             if index < top:
                 top = index
